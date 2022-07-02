b77933a5a595e0f134f7f24c86be4554a85a4f11
ShahinTheDeveloper <inboxtoshahin@gmail.com>
2022-07-02 17:28:32 +0600

    final upload

---
+++ app.js
@@ -0,0 +1,14 @@
+// var x =window.matchMedia("(max-width: 767px)")
+const list = document.querySelectorAll('.list');
+
+function activeLink (){
+	list.forEach((item) => {
+		item.classList.remove('active')});
+		this.classList.add('active');
+	
+	
+};
+list.forEach((item) => {
+	item.addEventListener('click',activeLink)
+
+});
